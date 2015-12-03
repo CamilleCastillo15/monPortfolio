6495560677fb2c33fdb096dfaea877ebfbb598da
Camille Castillo <camillecastillo15@gmail.com>
2015-12-03 23:52:56 +0100

    wip

---
+++ gulpfile.js
@@ -0,0 +1,128 @@
+/*
+ Gulp file config avec LibSass
+ Nécessite d'avoir node-sass installé
+ Les ressources sont stockées dans un dossier "assets" à la racine
+ Ce dossier contient :
+	## un dossier "scss" avec les sources sass
+	## un dossier "js" avec :
+		# un dossier "lib" qui contient les libs copié/collé sans aucune intervention
+			-> output : /include/js/lib/
+		# un dossier "plugins" dans lequels tous les fichiers seront concaténés
+			-> output : /include/scripts/scripts.min.js
+		# un dossier "script" dont le contenu sera linté puis concaténé à la suite des plugins
+			-> output : /include/scripts/scripts.min.js
+	## un dossier "images" dont le contenu sera optimisé
+		 -> output : /images
+ */
+
+// Include gulp
+var gulp = require('gulp'); 
+
+// Include Our Plugins
+var sass         = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss    = require('gulp-minify-css');
+var jshint       = require('gulp-jshint');
+var uglify       = require('gulp-uglify');
+var imagemin     = require('gulp-imagemin');
+var concat       = require('gulp-concat');
+var rename       = require('gulp-rename');
+var notify       = require('gulp-notify');
+var cache        = require('gulp-cache');
+var gutil        = require('gulp-util');
+var filter       = require('gulp-filter');
+var sourcemaps   = require('gulp-sourcemaps');
+var browserSync  = require('browser-sync');
+var reload       = browserSync.reload;
+var del          = require('del');
+
+gulp.task('browser-sync', function() {
+	browserSync({
+
+		/* ========================
+		 * RENSEIGNER LE VHOST ICI 
+		 * ======================== */
+
+		proxy: "monsite.dev",
+		open: false
+		
+	});
+});
+
+// Reload all Browsers
+gulp.task('bs-reload', function () {
+	browserSync.reload();
+});
+
+gulp.task('styles', function() {
+	gulp.src('assets/scss/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		.pipe(gulp.dest('assets/temp'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifycss({errLogToConsole: true}))
+		.pipe(gulp.dest('css'))
+		.pipe(filter('**/*.css'))
+		.pipe(reload({stream:true}));
+});
+
+gulp.task('lib', function() {
+	return gulp.src(['assets/js/lib/**/*.js'])
+		.pipe(gulp.dest('include/js/lib'));
+});
+
+gulp.task('lint', function() {
+	return gulp.src(['assets/js/scripts/zz-mcube.js'])
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
+
+gulp.task('scripts', function() {
+	return gulp.src(['assets/js/scripts/**/*.js'])
+		.pipe(sourcemaps.init())
+		.pipe(concat('scripts.js'))
+		.pipe(gulp.dest('assets/temp'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify().on('error', function(e) { console.log('\x07',e.message); return this.end(); }))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('include/js/scripts'))
+		.pipe(reload({stream:true}));
+});
+
+gulp.task('images', function() {
+	return gulp.src('assets/images/**/*')
+		.pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+		.pipe(gulp.dest('images'));
+});
+
+gulp.task('clearcache', function (done) {
+		return cache.clearAll(done);
+});
+
+gulp.task('clean', ['clearcache'], function(cb) {
+	del(['css', 'include/js', 'images'], cb)
+});
+
+gulp.task('build', ['clean'], function() {
+	gulp.start('styles', 'lint', 'lib', 'scripts', 'images');
+});
+
+gulp.task('default', ['browser-sync'], function() {
+	
+	// Watch .scss files
+	gulp.watch('assets/scss/**/*.scss', ['styles']);
+
+	// Watch our own JS files and lint
+	gulp.watch('assets/js/scripts/**/*.js', ['lint']);
+	
+	// Watch .js files
+	gulp.watch(['assets/js/scripts/**/*.js'], ['scripts', 'bs-reload']);
+
+	// Watch .js files
+	gulp.watch(['assets/js/lib/**/*.js'], ['lib', 'bs-reload']);
+	
+	// Watch image files
+	gulp.watch('assets/images/**/*', ['images']);
+
+	gulp.watch(['templates/**/*.tpl', '**/*.php', '**/*.html'], ['bs-reload']);
+});
--- index.php
+++ index.php
@@ -1,9 +1,43 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//FR">
-     <html>
-		<head>
-			<title>monsite</title>
-		</head>
-		 <body>
-			echo "lolilol";
-		 </body>
-     </html>
+<!doctype html>
+
+<html class="no-js">
+
+<head>
+	
+	<meta charset="utf-8">
+
+	<meta name="description" content="Portfolio de Camille Castillo, développeuse front - end" />
+
+	<meta name="keywords"    content="développement, front-end, camille, castillo, portfolio" />
+
+	<meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1.0,maximum-scale=1.0">
+
+	<title>Portfolio - Camille Castillo</title>
+
+	<!--[if (lt IE 9) & (!IEMobile)]>
+	<script src="/include/js/lib/nwmatcher-1.3.3.js"></script>
+	<script src="/include/js/lib/selectivizr-min.js"></script>
+	<![endif]-->
+
+	<!--[if (gt IE 8) | (IEMobile)]><!-->
+
+	<link rel="stylesheet" href="/css/screen.min.css">
+
+	<!--<![endif]-->
+	<!--[if (lt IE 9) & (!IEMobile)]>
+	<link rel="stylesheet" href="/css/screen-ie.min.css">
+	<![endif]-->
+
+	<script type="text/javascript" src="/include/js/lib/modernizr.js"></script>
+
+</head>
+
+	<body class="{$content}">
+		
+
+			
+
+		
+	</body>
+
+</html>
