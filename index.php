6badc47c50f47d1172a2dd1fe912a4ed9f74eb73
Camille Castillo <camillecastillo15@gmail.com>
2015-12-04 00:11:46 +0100

    ajout header -instable

--- index.php
+++ index.php
@@ -1,43 +1,15 @@
-<!doctype html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//FR">
+<link rel="stylesheet" href="/css/import.min.css">
+     <html>
+		<head>
+			<title>monsite</title>
+		</head>
+		 <body>
 
-<html class="no-js">
+		 	<div class="row">
+					<div class="header-content">
+					</div>
+			</div>
 
-<head>
-	
-	<meta charset="utf-8">
-
-	<meta name="description" content="Portfolio de Camille Castillo, développeuse front - end" />
-
-	<meta name="keywords"    content="développement, front-end, camille, castillo, portfolio" />
-
-	<meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1.0,maximum-scale=1.0">
-
-	<title>Portfolio - Camille Castillo</title>
-
-	<!--[if (lt IE 9) & (!IEMobile)]>
-	<script src="/include/js/lib/nwmatcher-1.3.3.js"></script>
-	<script src="/include/js/lib/selectivizr-min.js"></script>
-	<![endif]-->
-
-	<!--[if (gt IE 8) | (IEMobile)]><!-->
-
-	<link rel="stylesheet" href="/css/screen.min.css">
-
-	<!--<![endif]-->
-	<!--[if (lt IE 9) & (!IEMobile)]>
-	<link rel="stylesheet" href="/css/screen-ie.min.css">
-	<![endif]-->
-
-	<script type="text/javascript" src="/include/js/lib/modernizr.js"></script>
-
-</head>
-
-	<body class="{$content}">
-		
-
-			
-
-		
-	</body>
-
-</html>
+		 </body>
+     </html>
