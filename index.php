177f6ae3ed7562dc9a680243b5a478f557270288
Camille Castillo <camillecastillo15@gmail.com>
2015-12-04 15:19:20 +0100

    ajout premier paragraphe

--- index.php
+++ index.php
@@ -7,8 +7,29 @@
 		 <body>
 
 		 	<div class="row">
-					<div class="header-content">
+				<div class="header-content">
+					<h1 class="header-title">
+					<span class="title-span">Camille</span>
+					<span class="title-span">Castillo</span>
+					</h1>
+				</div>
+			</div>
+
+			<div class="row pad">
+				<div class="col-1-3 center-column">
+					<div class="center-1200-max"> 
+							<p>
+
+								Jeune diplômée dans le domaine du multimédia, je me suis spécialisée dans le domaine du développement front end.<br />
+								Mes études m’ont amenée à découvrir et approfondir de nombreux domaines, j’ai pu apprécier la liberté de créativité qu’offrait le monde du multimédia. <br />
+								A la croisée du design et du développement, le dévelopement front - end n’a eu cesse de piquer ma curiosité et de me passionner.<br />
+								Toujours à l’affût des dernières technologies qui sortent et permettent d’améliorer sa productivité, ma veille est constante.
+								Je suis acutellement à la recherche d’un stage dès le mois de mars 2015, n’hésitez pas à me contacter !
+
+
+							</p>
 					</div>
+				</div>
 			</div>
 
 		 </body>
