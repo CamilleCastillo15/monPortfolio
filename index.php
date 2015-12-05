42f55b407c94277439a4a4ab56739e6492224616
Camille Castillo <camillecastillo15@gmail.com>
2015-12-05 12:36:14 +0100

    header responsive avec bandes blanches pour garder la propriété de fusion du texte sur le fond

--- index.php
+++ index.php
@@ -4,33 +4,73 @@
 		<head>
 			<title>monsite</title>
 		</head>
+		<meta charset="utf-8">
 		 <body>
 
 		 	<div class="row">
 				<div class="header-content">
-					<h1 class="header-title">
+<!-- 					<h1 class="header-title">
 					<span class="title-span">Camille</span>
 					<span class="title-span">Castillo</span>
-					</h1>
+					</h1> -->
 				</div>
 			</div>
 
-			<div class="row pad">
-				<div class="col-1-3 center-column">
-					<div class="center-1200-max"> 
-							<p>
-
-								Jeune diplômée dans le domaine du multimédia, je me suis spécialisée dans le domaine du développement front end.<br />
-								Mes études m’ont amenée à découvrir et approfondir de nombreux domaines, j’ai pu apprécier la liberté de créativité qu’offrait le monde du multimédia. <br />
-								A la croisée du design et du développement, le dévelopement front - end n’a eu cesse de piquer ma curiosité et de me passionner.<br />
-								Toujours à l’affût des dernières technologies qui sortent et permettent d’améliorer sa productivité, ma veille est constante.
-								Je suis acutellement à la recherche d’un stage dès le mois de mars 2015, n’hésitez pas à me contacter !
-
-
-							</p>
+			<div class="row">
+				<div class="center-786-max"> 
+					<div class="col-1-3">
+						<div class="title-lev1">
+							Développeuse front - end
+						</div>
 					</div>
 				</div>
 			</div>
 
+			<div class="row pad">
+
+				<div class="center-786-max"> 
+
+					<div class="screen col-2-3">
+						<img src="images/screen.png" alt="screen" class="img-screen img" />
+					</div>
+
+					<div class="tablet col-1-3">
+						<img src="images/tablet.png" alt="tablette" class="img-tablet img" />
+					</div>
+
+					<div class="mobile col-1-3">
+						<img src="images/smartphone.png" alt="smartphone" class="img-mobile img" />
+					</div>
+
+				</div>
+
+			</div>
+
+			<div class="row pad">
+				<div class="col-3-3">
+					<div class="center-786-max"> 
+
+							<p class="texte_presentation">
+
+								&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+								Jeune diplômée dans le domaine du multimédia, je me suis spécialisée dans le domaine du développement front end.<br />
+								&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+								Mes études m’ont amenée à découvrir et approfondir de nombreux domaines, j’ai pu apprécier la liberté de créativité qu’offrait le monde du multimédia. <br />
+								&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+								A la croisée du design et du développement, le dévelopement front - end n’a eu cesse de piquer ma curiosité et de me passionner.<br />
+								Toujours à l’affût des dernières technologies qui sortent et permettent d’améliorer sa productivité, ma veille est constante.<br/>
+								&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+								Je suis acutellement à la recherche d’un stage dès le mois de mars 2015, n’hésitez pas à me contacter !
+
+
+							</p>
+
+					</div>
+
+				</div>
+
+			</div>
+
 		 </body>
+
      </html>
